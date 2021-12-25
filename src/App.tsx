e01d23b57671d8c41aa22d54fa71751e078a7afe
heycn <x1chennan@qq.com>
2021-12-25 14:17:52 +0800

    导航栏html

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
 import {HashRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Nav = styled.nav`
+  border: 1px solid blue;
+  ul {
+    display: flex;
+    li {
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path='/tags'>
+              <Tags />
+            </Route>
+            <Route path='/money'>
+              <Money />
+            </Route>
+            <Route path='/statistics'>
+              <Statistics />
+            </Route>
+            <Redirect exact from='/' to='/money' />
+            <Route path='*'>
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
           <ul>
             <li>
               <Link to='/tags'>标签</Link>
@@ -17,26 +58,8 @@ function App() {
               <Link to='/statistics'>统计</Link>
             </li>
           </ul>
-        </nav>
-
-        {/* 路由切换 */}
-        <Switch>
-          <Route path='/tags'>
-            <Tags />
-          </Route>
-          <Route path='/money'>
-            <Money />
-          </Route>
-          <Route path='/statistics'>
-            <Statistics />
-          </Route>
-          {/* 默认路由 */}
-          <Redirect exact from='/' to='/money' />
-          <Route path='*'>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
+        </Nav>
+      </Wrapper>
     </Router>
   );
 }
