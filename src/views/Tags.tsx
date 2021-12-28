db3440b6771467d5a6fe7b4d0c53234e41c6c34a
heycn <x1chennan@qq.com>
2021-12-28 21:13:05 +0800

    抽离 useTags 函数

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -5,7 +5,11 @@ function Tags() {
   const {tags, setTags} = useTags();
   return (
     <Layout>
-      <h2>标签页面</h2>
+      <ol>
+        {tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ol>
     </Layout>
   );
 }
