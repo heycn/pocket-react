483d8a0215687635dc0c1ad068eaee0e88d57c32
heycn <x1chennan@qq.com>
2021-12-29 17:48:37 +0800

    封装 findTag 到 useTags 里

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -12,7 +12,8 @@ const defaultTags = [
 
 const useTags = () => {
   const [tags, setTags] = React.useState<{id: number; name: string}[]>(defaultTags);
-  return {tags, setTags};
+  const findTag = (id: number) => tags.filter((tag) => tag.id === id)[0];
+  return {tags, setTags, findTag};
 };
 
 export {useTags};
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
+import {useTags} from 'useTags';
 
+type Params = {
+  id: string;
+};
 const Tag: React.FC = () => {
-  return (
-    <div>Tag 页面</div>
-  );
+  const {findTag} = useTags();
+  const {id} = useParams<Params>();
+  const tag = findTag(parseInt(id));
+  return <div>{tag.name}</div>;
 };
 
-export {Tag}
+export {Tag};
