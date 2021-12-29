56979596990fb948ff1b198159398d0ec29aa039
heycn <x1chennan@qq.com>
2021-12-29 12:40:08 +0800

    add Title

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,10 +1,18 @@
 import Layout from '../components/Layout';
 import {useTags} from 'useTags';
+import styled from 'styled-components';
+
+const Title = styled.section`
+  text-align: center;
+  padding: 14px 0;
+  font-size: 20px;
+`;
 
 function Tags() {
   const {tags, setTags} = useTags();
   return (
     <Layout>
+      <Title>标签</Title>
       <ol>
         {tags.map((tag) => (
           <li key={tag}>{tag}</li>
