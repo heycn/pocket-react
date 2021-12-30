56230f90ccaf4093c56a164cea7961e86f3fa40b
heycn <x1chennan@qq.com>
2021-12-30 20:03:47 +0800

    update

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -23,23 +23,11 @@ const useTags = () => {
     }
     return result;
   };
-  const updateTag = (id: number, obj: {name: string}) => {
-    // 获取你要改的 tag 的下标
-    const index = findTagIndex(id);
-    // 深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    // 把 tagsClone 的第 index 删掉，换成 {id: id, name: obj.name}
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
+  const updateTag = (id: number, {name}: {name: string}) => {
+    setTags(tags.map((tag) => (tag.id === id ? {id, name: name} : tag)));
   };
   const deleteTag = (id: number) => {
-    // 获取你要删的 tag 的下标
-    const index = findTagIndex(id);
-    // 深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    // 把 tagsClone 的第 index 删掉
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter((tag) => tag.id !== id));
   };
   return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
 };
