6a305f325452110af5f195916f263d213be962f9
heycn <x1chennan@qq.com>
2022-01-01 19:39:25 +0800

    update

---
+++ scripts/deploy_gitee.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git remote add origin git@gitee.com:heycn/pocket-react-website.git &&
+git push -u origin master -f
+cd -
